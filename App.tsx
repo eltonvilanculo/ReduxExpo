424a2a9a5aeadd2ce52f1dca81f29be13a099380
Elton Vilanculo <eltonvillas10@gmail.com>
2024-04-22 17:29:13 +0200

    Making a provider form redux

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { configureStore } from "@reduxjs/toolkit";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import appReducer from "./src/reducers";
+import { Provider, useSelector } from "react-redux";
+import MainPage from "./src/pages/MainPage";
 
 export default function App() {
+  const store = configureStore({
+    reducer: appReducer,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <MainPage />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
