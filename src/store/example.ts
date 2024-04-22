a7655ec4afed7059280974d29a120a61801ca728
Elton Vilanculo <eltonvillas10@gmail.com>
2024-04-22 17:32:57 +0200

    init commit

---
+++ src/store/example.ts
@@ -0,0 +1,25 @@
+// import {configureStore} from '@reduxjs/toolkit'
+
+// // Store -> Global state
+
+// // Action - just a name - increment
+// const increment  = ()=>{
+//     return {
+//         type: 'increment',
+//     }
+// }
+// const decrement  = ()=>{
+//     return {
+//         type: 'decrement',
+//     }
+// }
+
+// //reducer - take care of all the actions
+
+// const counter  = (state = 0 , action) =>{
+//     switch(action.type){
+//         case
+//     }
+// }
+
+// //Dispatch
